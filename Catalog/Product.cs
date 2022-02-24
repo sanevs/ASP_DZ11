5f4b7b0e1b3d42747b6c61315f0fbf35b2ef561f
Vaschenkov <vavland@mail.ru>
2022-02-24 20:21:19 +0300

    2402

--- Catalog/Product.cs
+++ Catalog/Product.cs
@@ -5,11 +5,14 @@ public class Product
     public string Name { get; }
     public int Price { get; set; }
     public Category Category { get; }
+    public string? ImageRef { get; set; }
+    
 
     public Product(string name, int price, Category category)
     {
         Name = name;
         Price = price;
         Category = category;
+        ImageRef = "product.jpg";
     }
 }
